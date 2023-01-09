1320edfc443a2a21176c444972b826b114136912
red-jet <jishnu_@outlook.com>
2023-01-09 21:26:04 +0530

    updated header file

--- Chapter_003/headerfiles.h
+++ Chapter_003/headerfiles.h
@@ -1,3 +1,6 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
 #include <iostream>
 #include <string>
 #include <string.h>
@@ -5,4 +8,9 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+
+#endif
